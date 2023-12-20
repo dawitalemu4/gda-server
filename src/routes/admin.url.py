cf2fed3eaf7b89acf3ed2ff059e13f815e4866aa
Michael Girma <michaelgirma@Michaels-MBP-3.fios-router.home>
2023-12-20 14:39:39 -0500

    finished routes and models

--- src/routes/admin.url.py
+++ src/routes/admin.url.py
@@ -3,7 +3,7 @@ def admin_urls():
     checkCredentials: str
 
     url = {
-        checkCredentials: "/auth/name/password"
+        checkCredentials: "/auth/name/password" 
     }
 
     return url
