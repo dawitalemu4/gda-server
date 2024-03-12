23669e6ab378f49a7b24695c26c9e0c76730e6fe
Dawit Alemu <dndawita@gmail.com>
2024-03-11 22:37:51 -0400

    removed admin page route

--- src/urls.py
+++ src/urls.py
@@ -1,5 +1,4 @@
 from django.urls import include, path
-from django.contrib import admin
 from rest_framework.routers import DefaultRouter
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
@@ -22,24 +21,22 @@ urlpatterns = [
 
     path('', include(router.urls)),
 
-    path('admin/', admin.site.urls),
-
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True, schema=schema))),
-    
+
     path(sale_clothes_urls["get_all_clothes"], sale_clothes_queries.as_view({'get': 'get_all_clothes'})),
     path(sale_clothes_urls["get_clothing_by_id"], sale_clothes_queries.as_view({'get': 'get_clothing_by_id'})),
     path(sale_clothes_urls["create_clothing"], sale_clothes_queries.as_view({'post': 'create_clothing'})),
     path(sale_clothes_urls["update_clothing"], sale_clothes_queries.as_view({'put': 'update_clothing'})),
     path(sale_clothes_urls["delete_clothing"], sale_clothes_queries.as_view({'delete': 'delete_clothing'})),
-    
+
     path(sold_clothes_urls["get_all_clothes"], sold_clothes_queries.as_view({'get': 'get_all_clothes'})),
     path(sold_clothes_urls["get_clothing_by_id"], sold_clothes_queries.as_view({'get': 'get_clothing_by_id'})),
     path(sold_clothes_urls["create_clothing"], sold_clothes_queries.as_view({'post': 'create_clothing'})),
     path(sold_clothes_urls["update_clothing"], sold_clothes_queries.as_view({'put': 'update_clothing'})),
     path(sold_clothes_urls["delete_clothing"], sold_clothes_queries.as_view({'delete': 'delete_clothing'})),
-    
+
     path(admin_urls["check_credentials"], admin_queries.as_view({'get': 'check_credentials'}))
-    
+
 ]
 
 urlpatterns += router.urls
